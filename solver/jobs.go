6092cbea96e1a465674db6d6274ffacf687b5c0b
Chris Goller <goller@gmail.com>
2023-06-02 11:05:46 -0500

    feat: log the vertex information for state failures
    Signed-off-by: Chris Goller <goller@gmail.com>

--- solver/jobs.go
+++ solver/jobs.go
@@ -515,7 +515,6 @@ func (jl *Solver) deleteIfUnreferenced(k digest.Digest, st *state) {
 		st.Release()
 		delete(jl.actives, k)
 	}
-
 }
 
 func (j *Job) Build(ctx context.Context, e Edge) (CachedResultWithProvenance, error) {
@@ -753,7 +752,7 @@ func (s *sharedOp) CalcSlowCache(ctx context.Context, index Index, p PreprocessF
 		if p != nil {
 			st := s.st.solver.getState(s.st.vtx.Inputs()[index])
 			if st == nil {
-				return "", errors.Errorf("failed to get state for index %d on %v", index, s.st.vtx.Name())
+				return "", errors.Errorf("failed to get state for index %d on %v %s", index, s.st.vtx.Name(), s.st.vtx.Digest())
 			}
 			ctx2 := progress.WithProgress(ctx, st.mpw)
 			if st.mspan.Span != nil {
